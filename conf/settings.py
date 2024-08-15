8ea64543c57824afe42a32d8e1a87199733c10a8
leejaeman2 <lividious2@naver.com>
2024-08-15 11:17:43 +0900

    add users app

--- conf/settings.py
+++ conf/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -81,3 +82,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
